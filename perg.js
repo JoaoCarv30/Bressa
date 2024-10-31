023302db5701b0160c7312718ab3aab390f67daf
Joao Victor <Carvalho.joao1@gmail.com>
2024-10-30 23:14:19 -0300

    Criei o projeto

---
+++ perg.js
@@ -0,0 +1,51 @@
+
+
+
+
+const perguntasFinanceiro = [
+    {
+        pergunta: "Você se sente bem orientado sobre prazos de pagamento e penalidades?",
+        alternativas: [
+            "Sim, totalmente, sempre recebo informações claras e precisas",
+            "Sim, na maioria das vezes, mas algumas informações poderiam ser mais detalhadas",
+            "Não muito, sinto falta de orientações mais claras sobre prazos e penalidades",
+            "Não satisfeito, não recebo orientações adequadas sobre prazos e penalidades"
+        ]
+    },
+    {
+        pergunta: "A disponibilidade dos funcionários para resolver problemas financeiros é:",
+        alternativas: [
+            "Excelente, sempre sou atendido prontamente",
+            "Boa, a maioria das vezes consigo resolver meus problemas",
+            "Regular, enfrento dificuldades em obter ajuda",
+            "Ruim, os funcionários não estão disponíveis quando preciso"
+        ]
+    },
+    {
+        pergunta: "A eficiência dos canais de comunicação para assuntos financeiros (e-mail, telefone, atendimento presencial) é:",
+        alternativas: [
+            "Muito eficiente, sempre obtive respostas rápidas e claras",
+            "Eficiente, na maioria das vezes recebo respostas satisfatórias",
+            "Pouco eficiente, demoro para obter respostas claras",
+            "Ineficiente, raramente consigo respostas rápidas e esclarecedoras"
+        ]
+    },
+    {
+        pergunta: "A flexibilidade para renegociações financeiras é:",
+        alternativas: [
+            "Excelente, sempre consigo uma solução que atenda minhas necessidades",
+            "Boa, geralmente há boas opções de renegociação",
+            "Regular, sinto que as opções são limitadas",
+            "Ruim, as opções de renegociação são muito restritas"
+        ]
+    },
+    {
+        pergunta: "Como você avalia o processo para solicitação de bolsas ou auxílios financeiros?",
+        alternativas: [
+            "Muito satisfatório, o processo é simples e eficiente",
+            "Satisfatório, geralmente consigo o que preciso, mas poderia ser mais fácil",
+            "Pouco satisfatório, enfrento algumas dificuldades no processo",
+            "Insatisfatório, o processo é complicado e demorado"
+        ]
+    }
+];
